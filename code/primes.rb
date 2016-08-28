5e3ea537440c3929bbbdfadba60674878d0d5895
Alexi Karuna <alexi@alexikaruna.com>
2016-08-28 15:55:15 -0500

    complete output and formatting

--- code/primes.rb
+++ code/primes.rb
@@ -1,5 +1,10 @@
 class Primes
 
+  def generate_output_matrix(input)
+    output = generate_multiplication_output_array(input)
+    add_headers_to_output_array(input, output)
+  end
+
   def generate_multiplication_output_array(input)
     output = []
 
@@ -26,26 +31,46 @@ class Primes
     output_array
   end
 
+  def print_output(output_array)
+    # set column width to the width of the last (largest) element of the matrix
+    last_row = output_array[output_array.length-1]
+    col_width = last_row[last_row.length-1].to_s.length
+
+    output_array.each do |row_columns|
+      row_text = ''
+
+      row_columns.each_with_index do |e, i|
+        row_text += " #{e.to_s.rjust(col_width)} |"
+      end
+      puts row_text
+    end
+  end
+
 #  def get_prime_numbers(how_many)
 #
 #  end
 #
-## input 'n',
-#  def sieve_of_eratosthene(size)
-#      arr=(0..size).to_a
-#      arr[0]=nil
-#      arr[1]=nil
-#      max=size
-#      (size/2+1).times do |n|
-#          if(arr[n]!=nil) then
-#              cnt=2*n
-#              while cnt <= max do
-#                  arr[cnt]=nil
-#                  cnt+=n
-#              end
-#          end
-#      end
-#      arr.compact!
-#  end
-#
+# input 'n',
+
+  # this is taken from:
+  # http://stackoverflow.com/questions/11673968/how-do-i-generate-the-first-n-prime-numbers
+  # ... dont really believe in reinventing the wheel if i can validate via tests that the
+  # code is reliable
+  def sieve_of_eratosthene(size)
+    arr=(0..size).to_a
+    arr[0]=nil
+    arr[1]=nil
+    max=size
+    (size/2+1).times do |n|
+        if(arr[n]!=nil) then
+            cnt=2*n
+            while cnt <= max do
+                arr[cnt]=nil
+                cnt+=n
+            end
+        end
+    end
+    arr.compact!
+  end
+
 end
--- test/primes_test.rb
+++ test/primes_test.rb
@@ -2,6 +2,16 @@ require 'test_helper'
 
 class PrimesTest < Test::Unit::TestCase
 
+  def test_get_n_prime_numbers
+    # from n input, decide how large input to sieve should be
+    # trim result to the desired...
+    # if not large enough, up the seed array request (optional param) and try again
+
+    assert_equal([2, 3, 5, 7, 11], get_n_prime_numbers(5))
+    assert_equal([2,3,5,7,11,13,17,19,23,29], get_n_prime_numbers(10))
+    assert_equal([2,3,5,7,11,13,17,19,23,29], get_n_prime_numbers(1000))
+  end
+
   def test_generate_multiplication_output_array
     primes = Primes.new
     input = [2,3,5,7,11,13,17,19,23,29]
@@ -23,26 +33,21 @@ class PrimesTest < Test::Unit::TestCase
     output_with_headers = primes.add_headers_to_output_array(input, output_array)
 
     assert_equal(4, output_with_headers.length)
-
     assert_equal(3, output_with_headers[2][0])
     output_with_headers.each { |i| assert_equal(4, i.length) }
   end
 
-#  def test_get_prime_numbers
-#    assert_equal([2, 3, 5, 7, 11], primes.get_prime_numbers(5))
-#  end
-#
-#  def test_sieve_of_eratosthene
-#    primes = Primes.new
-#
-#    assert_equal([], primes.sieve_of_eratosthene(0))
-#    assert_equal([], primes.sieve_of_eratosthene(1))
-#    assert_equal([2], primes.sieve_of_eratosthene(2))
-#    assert_equal([2,3], primes.sieve_of_eratosthene(3))
-#    assert_equal([2,3], primes.sieve_of_eratosthene(4))
-#    assert_equal([2,3,5], primes.sieve_of_eratosthene(5))
-#    assert_equal([2,3,5,7], primes.sieve_of_eratosthene(7))
-#    assert_equal([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97], primes.sieve_of_eratosthene(99))
-#  end
+  def test_sieve_of_eratosthene
+    primes = Primes.new
+
+    assert_equal([], primes.sieve_of_eratosthene(0))
+    assert_equal([], primes.sieve_of_eratosthene(1))
+    assert_equal([2], primes.sieve_of_eratosthene(2))
+    assert_equal([2,3], primes.sieve_of_eratosthene(3))
+    assert_equal([2,3], primes.sieve_of_eratosthene(4))
+    assert_equal([2,3,5], primes.sieve_of_eratosthene(5))
+    assert_equal([2,3,5,7], primes.sieve_of_eratosthene(7))
+    assert_equal([2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97], primes.sieve_of_eratosthene(99))
+  end
 
 end
